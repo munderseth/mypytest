3a8d84e7581be1e3df601462f7e5a6a36ba2faac
Mark Underseth <marku@s2technologies.com>
2017-06-02 02:40:08 +0000

    temp remove check for Coveralls

--- scripts/call-tox.bat
+++ scripts/call-tox.bat
@@ -1,5 +1,5 @@
 REM skip "coveralls" run in PRs or forks
-if "%TOXENV%" == "coveralls" (
+if "%TOXENV%" == "coverallsx" (
     if not defined COVERALLS_REPO_TOKEN (
         echo skipping coveralls run because COVERALLS_REPO_TOKEN is not defined
         exit /b 0
